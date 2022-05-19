2dfb6c9bc9c9297fc148b950b98c0e2f26482f5b
Ian Rumac <ian.rumac@outlook.com>
2022-05-19 16:05:00 +0200

    fix gradle deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,14 +13,24 @@ version = "1.0"
 repositories {
     mavenCentral()
     maven("https://jitpack.io")
-    mavenLocal()
+    val libs = listOf("flow-machine", "parser-multiplatform")
+    for (lib in libs) {
+        maven {
+            url = uri("https://maven.pkg.github.com/lotuslambda/${lib}")
+            credentials {
+                username = "ianrumac"
+                password = "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"
+            }
+        }
+    }
+
 }
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
     implementation("com.github.ajalt.clikt:clikt:3.4.1")
-    implementation("com.lotuslambda.engine:flow-machine:0-SNAPSHOT")
-    implementation("com.lotuslambda.parser:lotus-parser:0.0.1")
+    implementation("com.lotuslambda.engine:flow-machine:0.1.1-SNAPSHOT")
+    implementation("com.lotuslambda.parser:parsermultiplatform:0.1.0")
     implementation("io.ktor:ktor-server-core:1.6.8")
     implementation("io.ktor:ktor-server-cio:1.6.8")
     implementation("io.ktor:ktor-websockets:1.6.8")
